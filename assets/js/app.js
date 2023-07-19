b04799ab3b014fb849a2c13aeb18755bd46ec61e
svensglinz <svenglinz@gmail.com>
2023-07-19 09:34:46 +0200

    changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,15 @@
-'use strict';
+// Function to include the header.html content into the specified element
+function includeHeader() {
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            // Replace the content of the placeholder with the content of footer.html
+            document.getElementById('footer-placeholder').innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error fetching footer.html:', error);
+        });
+}
+
+// Call the function to include the header on each page
+includeHeader();
