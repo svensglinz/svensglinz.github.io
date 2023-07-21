3f392990e1211f5540ee509adc9a23eaca65529f
svensglinz <svenglinz@gmail.com>
2023-07-21 12:35:11 +0200

    changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,3 +15,19 @@ function includeFile(fileName, idName) {
 includeFile(fileName = '/footer.html', idName = 'footer');
 includeFile(fileName = '/header.html', idName = 'header');
 includeFile(fileName = '/social-media.html', idName = 'social-media-bar');
+
+
+/* script from here: 
+https://codepen.io/dudleystorey/pen/RRwryd
+to fade background !!!
+
+also need something to fade elements and pictures
+*/
+var nystories = document.querySelector("p").offsetTop;
+window.onscroll = function () {
+    if (window.pageYOffset > 0) {
+        var opac = (window.pageYOffset / nystories);
+        console.log(opac);
+        document.body.style.background = "linear-gradient(rgba(255, 255, 255, " + opac + "), rgba(255, 255, 255, " + opac + ")), url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/times-square-perspective.jpg) no-repeat";
+    }
+}
