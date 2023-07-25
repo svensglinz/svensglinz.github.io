ead5a3ffd4ec7db4441e107baa62afbc5964d412
svensglinz <svenglinz@gmail.com>
2023-07-25 19:34:21 +0200

    changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,8 +13,8 @@ function includeFile(fileName, idName) {
 
 // Call the function to include the header on each page
 /*includeFile(fileName = '/footer.html', idName = 'footer'); */
-includeFile(fileName = '/header.html', idName = 'header');
-includeFile(fileName = '/social-media.html', idName = 'social-media-bar');
+includeFile(fileName = 'header.html', idName = 'header');
+includeFile(fileName = 'social-media.html', idName = 'social-media-bar');
 
 
 /* script from here: 
@@ -26,32 +26,7 @@ also need something to fade elements and pictures
 
 document.addEventListener("DOMContentLoaded", function () {
 
-    /*
-    const div = document.getElementById('myDiv');
-
-    div.addEventListener('wheel', (e) => {
-        // Check if the user is scrolling vertically
-        if (Math.abs(e.deltaY) > Math.abs(e.deltaX)) {
-            // Adjust the scrollLeft property based on the vertical scroll direction
-            div.scrollLeft += e.deltaY;
-            // Prevent the default vertical scrolling behavior
-            e.preventDefault();
-        }
-    });
-*/
-    /* prevent default scrolling when the div comes into view! */
-    /*
-        const div = document.getElementById('myDiv');
-        div.addEventListener('wheel', (e) => {
-            // Check if the user is scrolling vertically
-            if (Math.abs(e.deltaY) > Math.abs(e.deltaX)) {
-                // Adjust the scrollLeft property based on the vertical scroll direction
-                div.scrollLeft += e.deltaY;
-                // Prevent the default vertical scrolling behavior
-                e.preventDefault();
-            }
-        });
-    */
+    /* observer for rectangles*/
     const observer = new IntersectionObserver(entries => {
         entries.forEach(entry => {
             const intersecting = entry.isIntersecting;
@@ -61,6 +36,23 @@ document.addEventListener("DOMContentLoaded", function () {
     },
         { threshold: 0, rootMargin: "-50px" }
     );
+
+    /*
+    /* problems: effect only once ? at least only if scrolling takes place from the top, not from the bottom!!! (eg. see the fade-in boxes!)*/
+    const observer_plane = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            const intersecting = entry.isIntersecting;
+            if (intersecting) {
+                entry.target.style.visibility = "visible";
+                entry.target.style.opacity = "0.3";
+                entry.target.style.transform = "scale(1) translateX(0px) translateY(0px)";
+            } else {
+                entry.target.style.visibility = "hidden";
+                entry.target.style.opacity = "0";
+                entry.target.style.transform = "scale(1.5) translateX(-500px) translateY(500px)";
+            }
+        });
+    }, { threshold: 1, rootMargin: "500px" });
 
     /*get all elements*/
     const elements = document.querySelectorAll(".fade-in");
@@ -76,16 +68,17 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
 
+
     document.addEventListener("scroll", function () {
         /*for background opacity*/
         const yScroll = window.scrollY;
         const windowHeight = window.innerHeight;
         const opac = yScroll / windowHeight;
-        document.body.style.setProperty('--scroll', opac)
+        document.body.style.setProperty('--scroll', 1 - opac)
+        console.log(opac)
     })
 
 });
-
 
 
 /* --> all elements shold fade out on the screen -> If the bottom of the elemnt is at the very top, opacity shoudl be at X). 
@@ -94,13 +87,15 @@ Same for images etc.. But they shoudl aso fadein when they coome into the viewpo
 also fade out intro image!
 /*may also want window.innterHeight --> To calculate the offset to the top of the window*/
 
+/*
 onLoadFunction = function () {
     document.body.style.opacity = '1';
-    document.querySelectorAll('.move-in-on-load').forEach(element => {
+    document.querySelectorAll('.fade-in-on-load').forEach(element => {
         element.style.transform = 'translateY(0)';
+        element.style.opacity = '1';
     })
 
 }
 
 window.onload = onLoadFunction;
-
+*/
