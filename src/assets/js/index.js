90de0bc02584956590adad005b2edceab22d0e10
svensglinz <svenglinz@gmail.com>
2024-02-21 18:37:36 +0100

    changes

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,7 +1,21 @@
+function centerSocialMedia(){
+
+    const socialMedia = document.querySelector('.social_media');
+    const greeting = document.querySelector('.greeting');
+    document.querySelector('.more').style.width = greeting.style.width;
+
+    const initialMargin = socialMedia.style.marginLeft;
+
+    if (window.innerWidth >= 756){
+        document.querySelector('.more').style.marginLeft = greeting.offsetLeft + 'px';
+        socialMedia.style.marginLeft = (greeting.clientWidth - socialMedia.clientWidth) / 2 + 'px';
+    } else {
+        socialMedia.style.marginLeft = initialMargin;
+        document.querySelector('.more').style.marginLeft =0;
+    }
+}
+//window.addEventListener('resize', () => centerSocialMedia());
+
 const socialMedia = document.querySelector('.social_media');
-const greeting = document.querySelector('.text .greeting');
-
-document.querySelector('.text .more').style.marginLeft = greeting.offsetLeft + 'px';
-socialMedia.style.marginLeft = (greeting.clientWidth - socialMedia.clientWidth) / 2 + 'px';
-
-
+const greeting = document.querySelector('.greeting');
+document.querySelector('.more').style.maxWidth = greeting.clientWidth - 35 + 'px';
